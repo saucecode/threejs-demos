98cac7cc22f083e6a46ca0254d4299d6fd67dd6b
Julian <saucecode@users.noreply.github.com>
2016-07-07 17:35:07 +1200

    full light and shadow code

--- 03_LightAndShadow/demo.js
+++ 03_LightAndShadow/demo.js
@@ -1,5 +1,5 @@
 var scene, camera, renderer, mesh;
-var meshFloor;
+var meshFloor, ambientLight, light;
 
 var keyboard = {};
 var player = { height:1.8, speed:0.2, turnSpeed:Math.PI*0.02 };
@@ -11,23 +11,40 @@ function init(){
 	
 	mesh = new THREE.Mesh(
 		new THREE.BoxGeometry(1,1,1),
-		new THREE.MeshBasicMaterial({color:0xff4444, wireframe:USE_WIREFRAME})
+		new THREE.MeshPhongMaterial({color:0xff4444, wireframe:USE_WIREFRAME})
 	);
 	mesh.position.y += 1;
+	mesh.receiveShadow = true;
+	mesh.castShadow = true;
 	scene.add(mesh);
 	
 	meshFloor = new THREE.Mesh(
 		new THREE.PlaneGeometry(10,10, 10,10),
-		new THREE.MeshBasicMaterial({color:0xffffff, wireframe:USE_WIREFRAME})
+		new THREE.MeshPhongMaterial({color:0xffffff, wireframe:USE_WIREFRAME})
 	);
 	meshFloor.rotation.x -= Math.PI / 2;
+	meshFloor.receiveShadow = true;
 	scene.add(meshFloor);
+	
+	ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+	scene.add(ambientLight);
+	
+	light = new THREE.PointLight(0xffffff, 0.8, 18);
+	light.position.set(-3,6,-3);
+	light.castShadow = true;
+	light.shadow.camera.near = 0.1;
+	light.shadow.camera.far = 25;
+	scene.add(light);
 	
 	camera.position.set(0, player.height, -5);
 	camera.lookAt(new THREE.Vector3(0,player.height,0));
 	
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize(1280, 720);
+
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.type = THREE.BasicShadowMap;
+	
 	document.body.appendChild(renderer.domElement);
 	
 	animate();
